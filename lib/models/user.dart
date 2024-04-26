65dd478af7f08725f3f4a54d4c84766768ea48c5
rositanndr <161114496+rositanndr@users.noreply.github.com>
2024-04-26 07:08:52 +0700

    Add files via upload

---
+++ lib/models/user.dart
@@ -0,0 +1,25 @@
+class User {
+  final int id;
+  final String email;
+  final String firstname;
+  final String lastname;
+  final String avatar;
+
+  User({
+    required this.id,
+    required this.email,
+    required this.firstname,
+    required this.lastname,
+    required this.avatar,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      email: json['email'],
+      firstname: json['first_name'], 
+      lastname: json['last_name'], 
+      avatar: json['avatar'],
+      );
+  }
+}
---
+++ lib/pages/home_page_stateless_future_builder.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:learnapi/services/user_service.dart';
+
+class HomePageStatelessFutureBuilder extends StatelessWidget {
+  const HomePageStatelessFutureBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Get Api Future Builder'
+        ),
+      ),
+      body: FutureBuilder(
+        future: UserService.fetchUsers(),
+         builder: (context, snapshot) {
+          final users = snapshot.data;
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator(),
+            );
+          }
+          
+          if (snapshot.hasError) {
+            return Center(child: Text('Error'),);
+          }
+
+          return ListView.builder(
+        itemCount: users!.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
+          return Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(user.avatar),
+              ),
+              title: Text('${user.firstname} ${user.lastname}'),
+              subtitle: Text(user.email),
+            ),
+          );
+        },
+      );
+         },
+         ), 
+          
+         
+    );
+  }
+}
